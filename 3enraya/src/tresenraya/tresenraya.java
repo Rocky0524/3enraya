3f3b523a4864cc99a502a26694dbfdd025d4f10b
Rocky0524 <kenbalboa05@gmail.com>
2024-12-09 16:30:40 +0100

    parte 1
    pregunto cules son los nombres de los jygadores y hago la moneda para que salga cara o cruz, declaro la matriz del juego

--- 3enraya/src/tresenraya/tresenraya.java
+++ 3enraya/src/tresenraya/tresenraya.java
@@ -9,8 +9,29 @@ public class tresenraya {
 		// TODO Auto-generated method stub
 Scanner s = new Scanner(System.in);
 Random random = new Random();		
-		
-		
+
+		//pregunta los nombres de los dos jugadores
+System.out.println("jugador 1 cual es tu nombre?");
+String jugador1 =s.next();
+System.out.println("jugador 2 cual es tu nombre?");
+String jugador2 =s.next();
+
+
+System.out.println("\n Ahora se tirara la moneda");
+
+int moneda= random.nextInt(2);
+if(moneda ==1) {
+	System.out.println("\n La moneda a salido cruz");
+}else {
+	System.out.println("\n La moneda a salido cara");
+}
+
+//matriz declarada para el juego
+	String matriz[][]= {{".", ".", "."},
+						{".", ".", "."},
+						{".", ".", "."}};	
+	
+
 	}
 
 }
