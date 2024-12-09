e1d1aa4cbfbf59c871cb9fb79cea4a7d4f7182a9
Rocky0524 <kenbalboa05@gmail.com>
2024-12-09 16:02:57 +0100

    inicial

---
+++ 3enraya/src/tresenraya/tresenraya.java
@@ -0,0 +1,16 @@
+package tresenraya;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class tresenraya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s = new Scanner(System.in);
+Random random = new Random();		
+		
+		
+	}
+
+}
